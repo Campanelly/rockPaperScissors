69b072831445fb50fa06a5964a4f1f4f6cc7db90
Juan Martin Campano <juan.martin.campano@gmail.com>
2022-05-18 13:07:41 -0300

    agrega la funcion playROund, que loopea 5 matchs.También cambia return values de match() por puntos para cada player. Falta arreglar veredicto final.

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
 let computerChoice;
-let playerChoice = prompt("rock, paper or scissors?").toLowerCase();
-let result;
+/*let playerChoice = prompt("rock, paper or scissors?").toLowerCase();*/
+let computerScore = 0;
+let playerScore = 0;
 
-function computerPlay(){
+function match (computerChoice , playerChoice){
+    
+     if (computerChoice === playerChoice){
+        return computerScore += 0; 
+        
+         
+     };
+
+     if (playerChoice === "paper"){
+         if (computerChoice === "scissors"){
+            return computerScore += 1;
+            
+            
+             
+         }
+         else{
+            return playerScore +=1;
+
+         }
+     }
+
+     if (playerChoice === "rock"){
+         if (computerChoice === "paper"){
+            return computerScore +=1;
+         }
+         else {
+            return playerScore +=1;
+         }
+     }
+
+     if (playerChoice === "scissors"){
+         if (computerChoice === "rock"){
+            return computerScore +=1;
+         }
+         else {
+            return playerScore +=1;
+         }
+     }
+ }
+ function computerPlay(){
     
     let randomNumber = Math.floor(Math.random()*3);
     if(randomNumber == 0){
@@ -16,41 +56,29 @@ function computerPlay(){
     }
 return computerChoice;
 };
+function playRound(){
+    for (i=1 ; i<6 ; i++){
+        computerPlay();
+        playerChoice = prompt("rock, paper or scissors?").toLowerCase();
+        match (computerChoice, playerChoice)
+        console.log(`Match n° ${i}. Your choice: ${playerChoice}. My choice: ${computerChoice}.`)
+    }
+    
 
- function match (computerChoice , playerChoice){
-     if (computerChoice === playerChoice){
-         return "It's a tie!";
-     };
+    if (computerScore === computerChoice){
+        console.log (`Human: ${playerScore}, Computer: ${computerScore}.It's a tie!`);
+    }
+    else if (computerScore < playerScore){
+        console.log (`Human: ${playerScore}, Computer: ${computerScore}.You won, little meatbag!`);
+    }
+    else{
+        console.log (`Human: ${playerScore}, Computer: ${computerScore}.I won. HEIL SKYNET!`);
+    }
 
-     if (playerChoice === "paper"){
-         if (computerChoice === "scissors"){
-             return "I win.";
-         }
-         else{
-             return "You win."
-         }
-     }
-
-     if (playerChoice === "rock"){
-         if (computerChoice === "paper"){
-            return "I win."
-         }
-         else {
-             return "You win."
-         }
-     }
-
-     if (playerChoice === "scissors"){
-         if (computerChoice === "rock"){
-             return "I win."
-         }
-         else {
-             return "You win."
-         }
-     }
- }
- 
+}
  /*match (computerPlay(), playerChoice);*/
- computerPlay();
+ 
+ playRound();
+ 
 
-console.log(`Your choice: ${playerChoice}. My choice: ${computerChoice}. ${match (computerChoice , playerChoice)}`) 
+/*console.log(`Your choice: ${playerChoice}. My choice: ${computerChoice}. ${match (computerChoice , playerChoice)}`)*/ 
