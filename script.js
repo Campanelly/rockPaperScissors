5d6bb8ecb4bcd2dcbb778365ba8a8997321c17a6
Juan Martin Campano <juan.martin.campano@gmail.com>
2022-06-18 11:27:01 -0300

    limpia el código

--- script.js
+++ script.js
@@ -52,25 +52,28 @@ const computerPlay = function computerPlay(){
 return computerChoice;
 };
 
-
-
 const rock = document.querySelector('#rock');
 rock.addEventListener('click',function(){
   playerChoice = 'rock'
   computerPlay();
   match(computerChoice,playerChoice);
+
   score.textContent = `Your choice: ${playerChoice}. My choice: ${computerChoice}.
     Score = Computer: ${computerScore}. Human: ${playerScore}.`;
+
   if(computerScore == 5){
-    document.querySelector('#rock').disabled = true;
-    document.querySelector('#paper').disabled = true;
-    document.querySelector('#scissors').disabled = true;
+    const buttons = document.querySelectorAll('button');
+      for (item of buttons){
+        item.disabled = true;
+      };
     veredict.textContent = 'I won. Heil Skynet!'
   }
   else if (playerScore == 5){
-    document.querySelector('#paper').disabled = true;
-    document.querySelector('#scissors').disabled = true;
-    document.querySelector('#rock').disabled = true;
+    
+    const buttons = document.querySelectorAll('button');
+      for (item of buttons){
+        item.disabled = true;
+      };
     veredict.textContent = 'You won, little meatbag.'
   };
   
@@ -81,18 +84,22 @@ paper.addEventListener('click',function(){
   playerChoice = 'paper'
   computerPlay();
   match(computerChoice,playerChoice);
+
   score.textContent = `Your choice: ${playerChoice}. My choice: ${computerChoice}.
     Score = Computer: ${computerScore}. Human: ${playerScore}.`;
+
     if(computerScore == 5){
-      document.querySelector('#rock').disabled = true;
-      document.querySelector('#paper').disabled = true;
-      document.querySelector('#scissors').disabled = true;
+      const buttons = document.querySelectorAll('button');
+      for (item of buttons){
+        item.disabled = true;
+      };
       veredict.textContent = 'I won. Heil Skynet!'
     }
     else if (playerScore == 5){
-      document.querySelector('#rock').disabled = true;
-      document.querySelector('#paper').disabled = true;
-      document.querySelector('#scissors').disabled = true;
+      const buttons = document.querySelectorAll('button');
+      for (item of buttons){
+        item.disabled = true;
+      };
       veredict.textContent = 'You won, little meatbag.'
     };
 });
@@ -102,40 +109,23 @@ scissors.addEventListener('click',function(){
   playerChoice = 'scissors'
   computerPlay();
   match(computerChoice,playerChoice);
+
   score.textContent = `Your choice: ${playerChoice}. My choice: ${computerChoice}.
     Score = Computer: ${computerScore}. Human: ${playerScore}.`;
+
     if(computerScore == 5){
-      document.querySelector('#paper').disabled = true;
-      document.querySelector('#scissors').disabled = true;
-      document.querySelector('#rock').disabled = true;
+      const buttons = document.querySelectorAll('button');
+      for (item of buttons){
+        item.disabled = true;
+      };      
       veredict.textContent = 'I won. Heil Skynet!'
     }
     else if (playerScore == 5){
-      document.querySelector('#paper').disabled = true;
-      document.querySelector('#scissors').disabled = true;
-      document.querySelector('#rock').disabled = true;
+      const buttons = document.querySelectorAll('button');
+      for (item of buttons){
+        item.disabled = true;
+      };
       veredict.textContent = 'You won, little meatbag.'
     };
 });
-
- 
- 
-  /*
-    if (computerScore === playerScore){
-        document.write (`<p>Human: ${playerScore}, Computer: ${computerScore}. It's a tie!</p>`);
-    }
-    else if (computerScore < playerScore){
-        document.write(`<p>Human: ${playerScore}, Computer: ${computerScore}. You won, little meatbag!</p>`);
-    }
-    else{
-        document.write (`<p>Human: ${playerScore}, Computer: ${computerScore}. I won. HEIL SKYNET!</p>`);
-    }
-
-}
-
- 
- playRound();
-
- 
-*/
  
